977d1846fba2d7ecc27fa89c0bd7ff392cb6fbd8
Mateusz <mateusz.kordowiak@gmail.com>
2024-01-29 22:37:40 +0100

    Tests

---
+++ FinansoApp.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using FinansoData.Data;
+using FinansoData.Models;
+
+namespace FinansoApp.Tests.Controllers
+{
+    public class AccountControllerTest
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountControllerTest()
+        {
+            
+        }
+        [Fact]
+        public void AccountController_Login_ShoudLoginWhenCredentialsAreCorrect()
+        {
+            var www = new DBContextBuilder<ApplicationDbContext>();
+        }
+    }
+}
