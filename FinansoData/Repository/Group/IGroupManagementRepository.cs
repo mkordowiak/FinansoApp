d9397d8643106364070e4c46e485748c8d9d99bc
Mateusz <mateusz.kordowiak@gmail.com>
2024-11-07 12:38:05 +0100

    Delete group members

--- FinansoData/Repository/Group/IGroupManagementRepository.cs
+++ FinansoData/Repository/Group/IGroupManagementRepository.cs
@@ -2,6 +2,34 @@
 {
     public interface IGroupManagementRepository
     {
+        /// <summary>
+        /// Create new group
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <param name="appUser"></param>
+        /// <returns></returns>
         Task<RepositoryResult<bool?>> Add(string groupName, string appUser);
+
+        /// <summary>
+        /// Remove single user from gorup
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <param name="appUser"></param>
+        /// <returns></returns>
+        Task<RepositoryResult<bool>> DeleteGroupUser(int groupId, string appUser);
+
+        /// <summary>
+        /// Removes all users from group
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <returns></returns>
+        Task<RepositoryResult<bool>> DeleteAllGroupUsers(int groupId);
+
+        /// <summary>
+        /// Removes group
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <returns></returns>
+        Task<RepositoryResult<bool>> DeleteGroup(int groupId);
     }
 }
