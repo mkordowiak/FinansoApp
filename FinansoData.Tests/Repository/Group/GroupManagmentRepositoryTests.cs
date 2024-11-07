7ba235d24c031ad58062c81b383d1e80f9d7b8f3
Mateusz <mateusz.kordowiak@gmail.com>
2024-11-07 16:21:51 +0100

    DeleteGroup

--- FinansoData.Tests/Repository/Group/GroupManagmentRepositoryTests.cs
+++ FinansoData.Tests/Repository/Group/GroupManagmentRepositoryTests.cs
@@ -1,23 +1,18 @@
 ﻿using FinansoData.Data;
 using FinansoData.Models;
-using FinansoData.Repository;
 using FinansoData.Repository.Group;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FinansoData.Tests.Repository.Group
 {
     public class GroupManagmentRepositoryTests
     {
         private readonly DbContextOptions<ApplicationDbContext> _dbContextOptions;
-        private AppUser _groupOwner;
-        private AppUser _groupMember;
-        private FinansoData.Models.Group _group;
+        private AppUser _group1Owner;
+        private AppUser _group1Member;
+        private FinansoData.Models.Group _group1;
+        private Models.Group _group2;
         private GroupUser _groupUser;
 
         public GroupManagmentRepositoryTests()
@@ -32,16 +27,20 @@ namespace FinansoData.Tests.Repository.Group
                 context.Database.EnsureCreated();
 
 
-                _groupOwner = new AppUser { Id = "1", UserName = "john@mail.com", FirstName = "John", LastName = "Doe" };
-                _groupMember = new AppUser { Id = "2", UserName = "mark@mail.com", FirstName = "Mark", LastName = "Knopfler" };
+                _group1Owner = new AppUser { Id = "1", UserName = "john@mail.com", FirstName = "John", LastName = "Doe" };
+                _group1Member = new AppUser { Id = "2", UserName = "mark@mail.com", FirstName = "Mark", LastName = "Knopfler" };
 
-                _group = new FinansoData.Models.Group { Id = 1, Name = "Test _group 1", OwnerAppUser = _groupOwner };
+                _group1 = new FinansoData.Models.Group { Id = 1, Name = "Test _group 1", OwnerAppUser = _group1Owner };
+                _group2 = new FinansoData.Models.Group { Id = 2, Name = "Test _group 2", OwnerAppUser = _group1Owner };
 
-                _groupUser = new GroupUser { Id = 1, Group = _group, AppUser = _groupMember };
 
-                context.AppUsers.Add(_groupOwner);
-                context.AppUsers.Add(_groupMember);
-                context.Groups.Add(_group);
+
+                _groupUser = new GroupUser { Id = 1, Group = _group1, AppUser = _group1Member };
+
+                context.AppUsers.Add(_group1Owner);
+                context.AppUsers.Add(_group1Member);
+                context.Groups.Add(_group1);
+                context.Groups.Add(_group2);
                 context.GroupUsers.Add(_groupUser);
 
                 context.SaveChanges();
@@ -57,9 +56,9 @@ namespace FinansoData.Tests.Repository.Group
             {
                 IGroupCrudRepository groupCrudRepository = new GroupCrudRepository(context);
                 GroupManagementRepository groupRepository = new GroupManagementRepository(context, groupCrudRepository);
-                
+
                 string newGroupName = "New group";
-                string groupOwnerUserName = _groupOwner.UserName;
+                string groupOwnerUserName = _group1Owner.UserName;
 
                 // Act
                 FinansoData.RepositoryResult<bool?> data = await groupRepository.Add(newGroupName, groupOwnerUserName);
@@ -72,19 +71,19 @@ namespace FinansoData.Tests.Repository.Group
         }
 
         [Fact]
-        public async Task GroupRepository_Add_ShoudBeFailureIfWrongUserId()
+        public async Task GroupManagementRepository_Add_ShoudBeFailureIfWrongUserId()
         {
             // Arrange
             using (ApplicationDbContext context = new ApplicationDbContext(_dbContextOptions))
             {
                 IGroupCrudRepository groupCrudRepository = new GroupCrudRepository(context);
-                GroupManagementRepository groupRepository = new GroupManagementRepository(context, groupCrudRepository);
+                GroupManagementRepository groupManagementRepository = new GroupManagementRepository(context, groupCrudRepository);
 
                 string newGroupName = "New group";
                 string groupOwnerUserName = "wrong username";
 
                 // Act
-                FinansoData.RepositoryResult<bool?> data = await groupRepository.Add(newGroupName, groupOwnerUserName);
+                FinansoData.RepositoryResult<bool?> data = await groupManagementRepository.Add(newGroupName, groupOwnerUserName);
                 // Destroy in-memory database to prevent running multiple instance
                 context.Database.EnsureDeleted();
 
@@ -94,6 +93,50 @@ namespace FinansoData.Tests.Repository.Group
             }
         }
 
+        [Fact]
+        public async Task GroupManagementRepository_DeleteGroup_ShouldRemoveGroup()
+        {
+            // Arrange
+            using (ApplicationDbContext context = new ApplicationDbContext(_dbContextOptions))
+            {
+                IGroupCrudRepository groupCrudRepository = new GroupCrudRepository(context);
+                GroupManagementRepository groupManagementRepository = new GroupManagementRepository(context, groupCrudRepository);
 
+                // Act
+                RepositoryResult<bool> result = await groupManagementRepository.DeleteGroup(_group1.Id);
+                
+                
+
+                // Assert 
+                result.IsSuccess.Should().BeTrue();
+                context.Groups.Should().HaveCount(1);
+                context.Groups.FirstOrDefault().Id.Should().Be(2);
+
+                // Destroy in-memory database to prevent running multiple instance
+                context.Database.EnsureDeleted();
+            }
+        }
+
+
+        [Fact]
+        public async Task GroupManagementRepository_DeleteGroup_ShouldRetrurnFailureWhenGroupIsIdIsIncorrect()
+        {
+            // Arrange
+            using (ApplicationDbContext context = new ApplicationDbContext(_dbContextOptions))
+            {
+                IGroupCrudRepository groupCrudRepository = new GroupCrudRepository(context);
+                GroupManagementRepository groupManagementRepository = new GroupManagementRepository(context, groupCrudRepository);
+
+                // Act
+                RepositoryResult<bool> result = await groupManagementRepository.DeleteGroup(999);
+                // Destroy in-memory database to prevent running multiple instance
+                context.Database.EnsureDeleted();
+
+                // Assert 
+                result.IsSuccess.Should().BeFalse();
+                result.ErrorType.Should().Be(ErrorType.NotFound);
+                
+            }
+        }
     }
 }
