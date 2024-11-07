97f8d87f86b372ba09b31fb9164431f33d0dabd3
Mateusz <mateusz.kordowiak@gmail.com>
2024-11-07 11:22:05 +0100

    methods summary

--- FinansoData/Repository/Group/IGroupQueryRepository.cs
+++ FinansoData/Repository/Group/IGroupQueryRepository.cs
@@ -4,9 +4,38 @@ namespace FinansoData.Repository.Group
 {
     public interface IGroupQueryRepository
     {
+
+        /// <summary>
+        /// Get all groups for user
+        /// </summary>
+        /// <param name="appUser"></param>
+        /// <returns></returns>
         Task<RepositoryResult<IEnumerable<GetUserGroupsViewModel>?>> GetUserGroups(string appUser);
+
+        /// <summary>
+        /// Get all group members
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<RepositoryResult<IEnumerable<GetGroupMembersViewModel>>> GetGroupMembersAsync(int id);
+
+        /// <summary>
+        /// If user is group owner
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <param name="appUser"></param>
+        /// <returns></returns>
         Task<RepositoryResult<bool>> IsUserGroupOwner(int groupId, string appUser);
+
+        /// <summary>
+        /// User group membership
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <param name="appUser"></param>
+        /// <returns>
+        /// IsOwner
+        /// IsMember
+        /// </returns>
         Task<RepositoryResult<GetUserMembershipInGroupViewModel>> GetUserMembershipInGroupAsync(int groupId, string appUser);
     }
 
