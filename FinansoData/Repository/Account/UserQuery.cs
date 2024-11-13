06eef6d006aca807db1b0b259de911dd835d9365
Mateusz <mateusz.kordowiak@gmail.com>
2024-11-13 21:12:23 +0100

    search for users

---
+++ FinansoData/Repository/Account/UserQuery.cs
@@ -0,0 +1,31 @@
+﻿using FinansoData.Data;
+using FinansoData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinansoData.Repository.Account
+{
+    public class UserQuery : IUserQuery
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserQuery(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RepositoryResult<AppUser?>> GetUserByEmail(string email)
+        {
+            AppUser? result;
+            try
+            {
+                result = await _context.Users.Where(x => x.Email == email).SingleOrDefaultAsync();
+            }
+            catch
+            {
+                return RepositoryResult<AppUser?>.Failure(null, ErrorType.ServerError);
+            }
+
+            return RepositoryResult<AppUser?>.Success(result);
+        }
+    }
+}
