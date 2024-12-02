0f4b59c37ce191e222f2121b7cffc42686b8742c
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-02 17:00:02 +0100

    NewItemShouldNotBeActive

--- FinansoData.Tests/Repository/Group/GroupUsersManagementRepositoryTests.cs
+++ FinansoData.Tests/Repository/Group/GroupUsersManagementRepositoryTests.cs
@@ -94,7 +94,7 @@ namespace FinansoData.Tests.Repository.Group
         }
 
         [Fact]
-        public async Task GroupManagementRepository_AddUserToGroup_ShouldReturnnnn()
+        public async Task GroupManagementRepository_AddUserToGroup_ShouldAddItem()
         {
             // Arrange
             using (ApplicationDbContext context = new ApplicationDbContext(_dbContextOptions))
@@ -107,6 +107,26 @@ namespace FinansoData.Tests.Repository.Group
 
                 // Assert
                 context.GroupUsers.Should().HaveCount(2);
+
+                // Destroy in-memory database to prevent running multiple instance
+                context.Database.EnsureDeleted();
+            }
+        }
+
+        [Fact]
+        public async Task GroupManagementRepository_AddUserToGroup_NewItemShouldNotBeActive()
+        {
+            // Arrange
+            using (ApplicationDbContext context = new ApplicationDbContext(_dbContextOptions))
+            {
+                IGroupCrudRepository groupCrudRepository = new GroupCrudRepository(context);
+                GroupUsersManagementRepository groupManagementRepository = new GroupUsersManagementRepository(context, groupCrudRepository);
+
+                // Act
+                RepositoryResult<bool> result = await groupManagementRepository.AddUserToGroup(_group1, _group1Member);
+
+                // Assert
+                context.GroupUsers.Last().Active.Should().BeFalse();
 
                 // Destroy in-memory database to prevent running multiple instance
                 context.Database.EnsureDeleted();
