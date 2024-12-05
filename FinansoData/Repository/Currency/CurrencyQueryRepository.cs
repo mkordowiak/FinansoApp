f66f327592a7e991368a7e1f951677adc0a0ff2c
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-05 14:34:33 +0100

    Currency - new repo

---
+++ FinansoData/Repository/Currency/CurrencyQueryRepository.cs
@@ -0,0 +1,71 @@
+﻿using FinansoData.Data;
+using FinansoData.DataViewModel.Currency;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinansoData.Repository.Currency
+{
+    public class CurrencyQueryRepository : ICurrencyQueryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CurrencyQueryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RepositoryResult<IEnumerable<CurrencyViewModel>>> GetAllCurrencies()
+        {
+            IQueryable<CurrencyViewModel> query = from currency in _context.Currencies
+                                                       select new CurrencyViewModel
+                                                       {
+                                                           Id = currency.Id,
+                                                           Name = currency.Name
+                                                       };
+
+            try
+            {
+                return RepositoryResult<IEnumerable<CurrencyViewModel>>.Success(await query.ToListAsync());
+            }
+            catch
+            {
+                return RepositoryResult<IEnumerable<CurrencyViewModel>>.Failure(null, ErrorType.ServerError);
+            }
+        }
+
+        public async Task<RepositoryResult<Models.Currency?>> GetCurrencyModelById(int id)
+        {
+            try
+            {
+                RepositoryResult<Models.Currency>? result = await _context.Currencies.Where(x => x.Id == id)
+                    .Select(x => RepositoryResult<Models.Currency>.Success(x))
+                    .SingleOrDefaultAsync();
+
+                return result;
+            }
+            catch
+            {
+                return RepositoryResult<Models.Currency>.Failure(null, ErrorType.ServerError);
+            }
+        }
+
+        public async Task<RepositoryResult<CurrencyViewModel>> GetCurrencyById(int id)
+        {
+            try
+            {
+                RepositoryResult<CurrencyViewModel>? result = await _context.Currencies.Where(x => x.Id == id)
+                    .Select(x => new CurrencyViewModel
+                    {
+                        Id = x.Id,
+                        Name = x.Name
+                    })
+                    .Select(x => RepositoryResult<CurrencyViewModel>.Success(x))
+                    .SingleOrDefaultAsync();
+
+                return result;
+            }
+            catch
+            {
+                return RepositoryResult<CurrencyViewModel>.Failure(null, ErrorType.ServerError);
+            }
+        }
+    }
+}
---
+++ FinansoData/Repository/Currency/ICurrencyQueryRepository.cs
@@ -0,0 +1,22 @@
+﻿using FinansoData.DataViewModel.Currency;
+
+namespace FinansoData.Repository.Currency
+{
+    public interface ICurrencyQueryRepository
+    {
+        /// <summary>
+        /// Get all currencies
+        /// </summary>
+        /// <returns></returns>
+        Task<RepositoryResult<IEnumerable<CurrencyViewModel>>> GetAllCurrencies();
+
+        /// <summary>
+        /// Get currency by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<RepositoryResult<CurrencyViewModel>> GetCurrencyById(int id);
+
+        Task<RepositoryResult<Models.Currency?>> GetCurrencyModelById(int id);
+    }
+}
