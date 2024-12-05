85bd7674c28e95b4a0453ed365f11e15e8d46b54
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-05 14:34:10 +0100

    Balance - new  repo

---
+++ FinansoData/DataViewModel/Balance/BalanceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FinansoData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinansoData.DataViewModel.Balance
+{
+    public class BalanceViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public Models.Currency Currency { get; set; }
+        public Models.Group Group { get; set; }
+    }
+}
---
+++ FinansoData/Repository/Balance/BalanceManagementRepository.cs
@@ -0,0 +1,53 @@
+﻿using FinansoData.Data;
+using FinansoData.DataViewModel.Balance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinansoData.Repository.Balance
+{
+    public class BalanceManagementRepository : IBalanceManagmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BalanceManagementRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RepositoryResult<bool>> AddBalance(BalanceViewModel balance)
+        {
+            var newBalance = new Models.Balance
+            {
+                Name = balance.Name,
+                Amount = balance.Amount,
+                Currency = balance.Currency,
+                Group = balance.Group
+            };
+
+            try
+            {
+                _context.Balances.Add(newBalance);
+                await _context.SaveChangesAsync();
+
+                return RepositoryResult<bool>.Success(true);
+            }
+            catch
+            {
+                return RepositoryResult<bool>.Failure(null, ErrorType.ServerError);
+            }
+        }
+
+        public Task<RepositoryResult<bool>> DeleteBalance(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RepositoryResult<bool>> UpdateBalance(BalanceViewModel balance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
