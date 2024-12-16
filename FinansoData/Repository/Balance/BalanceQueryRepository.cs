05b2bb5f00ff8220041bc6176acaca66b179c562
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-16 12:17:59 +0100

    List of balances for group and user

---
+++ FinansoData/Repository/Balance/BalanceQueryRepository.cs
@@ -0,0 +1,82 @@
+﻿using FinansoData.Data;
+using FinansoData.DataViewModel.Balance;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinansoData.Repository.Balance
+{
+    public class BalanceQueryRepository : IBalanceQueryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BalanceQueryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RepositoryResult<IEnumerable<BalanceViewModel>?>> GetListOfBalancesForGroup(int groupId)
+        {
+            IQueryable<BalanceViewModel> query = from g in _context.Groups
+                                                 join b in _context.Balances on g.Id equals b.Group.Id
+                                                 where g.Id == groupId
+                                                 select new BalanceViewModel
+                                                 {
+                                                     Id = b.Id,
+                                                     Name = b.Name,
+                                                     Amount = b.Amount,
+                                                     Currency = b.Currency,
+                                                     Group = b.Group
+                                                 };
+
+            try
+            {
+                List<BalanceViewModel> result = await query.ToListAsync();
+                return RepositoryResult<IEnumerable<BalanceViewModel>>.Success(result);
+            }
+            catch
+            {
+                return RepositoryResult<IEnumerable<BalanceViewModel>>.Failure(null, ErrorType.ServerError);
+            }
+        }
+
+        public async Task<RepositoryResult<IEnumerable<BalanceViewModel>?>> GetListOfBalancesForUser(string userName)
+        {
+            IQueryable<BalanceViewModel> queryGroupMember = from u in _context.Users
+                                                            join gu in _context.GroupUsers on u.Id equals gu.AppUserId
+                                                            join g in _context.Groups on gu.Group.Id equals g.Id
+                                                            join b in _context.Balances on g.Id equals b.Group.Id
+                                                            where u.UserName == userName
+                                                            select new BalanceViewModel
+                                                            {
+                                                                Id = b.Id,
+                                                                Name = b.Name,
+                                                                Amount = b.Amount,
+                                                                Currency = b.Currency,
+                                                                Group = b.Group
+                                                            };
+
+            IQueryable<BalanceViewModel> queryGroupOwner = from u in _context.Users
+                                                               //join gg in _context.Groups on u.Id equals gg.OwnerAppUser.Id
+                                                           join g in _context.Groups on u.Id equals g.OwnerAppUser.Id
+                                                           join b in _context.Balances on g.Id equals b.Group.Id
+                                                           where u.UserName == userName
+                                                           select new BalanceViewModel
+                                                           {
+                                                               Id = b.Id,
+                                                               Name = b.Name,
+                                                               Amount = b.Amount,
+                                                               Currency = b.Currency,
+                                                               Group = b.Group
+                                                           };
+
+            try
+            {
+                List<BalanceViewModel> result = await queryGroupMember.Union(queryGroupOwner).ToListAsync();
+                return RepositoryResult<IEnumerable<BalanceViewModel>>.Success(result);
+            }
+            catch
+            {
+                return RepositoryResult<IEnumerable<BalanceViewModel>>.Failure(null, ErrorType.ServerError);
+            }
+        }
+    }
+}
---
+++ FinansoData/Repository/Balance/IBalanceQueryRepository.cs
@@ -0,0 +1,26 @@
+﻿using FinansoData.DataViewModel.Balance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinansoData.Repository.Balance
+{
+    public interface IBalanceQueryRepository
+    {
+        /// <summary>
+        /// Get list of balances for user
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns>IEnumerable of model</returns>
+        Task<RepositoryResult<IEnumerable<BalanceViewModel>?>> GetListOfBalancesForUser(string userName);
+
+        /// <summary>
+        /// Get list of balances for group
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <returns>IEnumerable of model</returns>
+        Task<RepositoryResult<IEnumerable<BalanceViewModel>?>> GetListOfBalancesForGroup(int groupId);
+    }
+}
