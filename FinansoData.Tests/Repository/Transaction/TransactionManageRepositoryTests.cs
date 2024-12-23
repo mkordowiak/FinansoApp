829ee9df229592dc79a119426dbfcf491aec8c30
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-24 00:58:39 +0100

    Get transactions repo tests

---
+++ FinansoData.Tests/Repository/Transaction/TransactionManageRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinansoData.Tests.Repository.Transaction
+{
+    public class TransactionManageRepositoryTests
+    {
+        public TransactionManageRepositoryTests()
+        {
+            
+        }
+
+        #region GetAllTransactionsForGroup
+
+        [Fact]
+        public async Task TransactionManageRepository_GetAllTransactionsForGroupFromDB()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public async Task TransactionManageRepository_GetAllTransactionsForGroupFromCache()
+        {
+            Assert.True(false);
+        }
+
+        #endregion
+
+
+        #region
+
+        [Fact]
+        public async Task TransactionManageRepository_GetAllTransactionsForBalanceFromDB()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public async Task TransactionManageRepository_GetAllTransactionsForBalanceFromCache()
+        {
+            Assert.True(false);
+        }
+
+        #endregion
+    }
+}
