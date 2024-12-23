8ac1b5c2c03b435ba62db29cd3a203f11aa6efe0
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-24 00:56:49 +0100

    Repo prototype

---
+++ FinansoData/Repository/TransactionStatus/ITransactionStatusQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace FinansoData.Repository.TransactionStatus
+{
+    public interface ITransactionStatusQueryRepository
+    {
+        Task<List<Models.TransactionStatus>> GetAllTransactionStatus();
+        Task<Models.TransactionStatus> GetTransactionStatusById(int id);
+        Task<Models.TransactionStatus> GetTransactionStatusByName(string name);
+    }
+}
