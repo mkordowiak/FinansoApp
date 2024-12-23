c8c9719827dd058e71acb2c98d6cb2096136c720
Mateusz <mateusz.kordowiak@gmail.com>
2024-12-24 00:33:55 +0100

    GetAllTransactionsForGroup

---
+++ FinansoData/Repository/Transaction/ITransactionManageRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace FinansoData.Repository.Transaction
+{
+    public interface ITransactionManageRepository
+    {
+        Task<RepositoryResult<IEnumerable<Models.BalanceTransaction>?>> GetAllTransactionsForGroup(int groupId);
+        Task<RepositoryResult<IEnumerable<Models.BalanceTransaction>?>> GetAllTransactionsForBalance(int balanceId);
+    }
+}
