777f83aeaf3f8e52532a9414b4b58b4dc4a3da7f
Mateusz <mateusz.kordowiak@gmail.com>
2025-01-06 21:19:23 +0100

    prevent from storing / getting cache when debbuging

--- FinansoData/Repository/CacheWrapper.cs
+++ FinansoData/Repository/CacheWrapper.cs
@@ -18,12 +18,22 @@ namespace FinansoData.Repository
 
         public void Set(string key, object value, TimeSpan cacheDuration)
         {
+#if DEBUG
+            return;
+#else
             _memoryCache.Set(key, value, cacheDuration);
+#endif
         }
 
         public bool TryGetValue<T>(string key, out T value)
         {
+#if DEBUG
+            value = default;
+            return false;
+
+#else
             return _memoryCache.TryGetValue(key, out value);
+#endif
         }
     }
 }
