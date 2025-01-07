e54801896db7cc60fb5bcbc5d1e41d7e6f13de19
Mateusz <mateusz.kordowiak@gmail.com>
2025-01-07 20:26:28 +0100

    Transaction management repo

---
+++ FinansoData/Repository/Transaction/ITransactionManagementRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace FinansoData.Repository.Transaction
+{
+    public interface ITransactionManagementRepository
+    {
+        Task<RepositoryResult<bool>> AddTransaction(decimal Amount, string? Description, int BalanceId, DateTime TransactionDate, string UserName, int TransactionTypeId, int TransactionStatusId);
+    }
+}
