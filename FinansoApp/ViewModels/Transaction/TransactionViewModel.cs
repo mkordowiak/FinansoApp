1bfee9668438e36b8019f1d1b440b39b43765517
Mateusz <mateusz.kordowiak@gmail.com>
2025-01-08 15:53:21 +0100

    Transaction List ViewModels

---
+++ FinansoApp/ViewModels/Transaction/TransactionViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FinansoApp.ViewModels.Transaction
+{
+    public class TransactionViewModel
+    {
+        public int Id { get; set; }
+        public string BalanceName { get; set; }
+        public string GroupName { get; set; }
+        public int TransactionId { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string TransactionStatus { get; set; }
+        public string TransactionType { get; set; }
+
+    }
+}
