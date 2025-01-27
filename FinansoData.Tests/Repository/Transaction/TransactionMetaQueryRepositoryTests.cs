05482db97cd1ddd79f114455bbc01ce2e003fec9
Mateusz <mateusz.kordowiak@gmail.com>
2025-01-27 18:06:21 +0100

    Create new repo

---
+++ FinansoData.Tests/Repository/Transaction/TransactionMetaQueryRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using FinansoData.Data;
+using FinansoData.DataViewModel.Transaction;
+using FinansoData.Models;
+using FinansoData.Repository;
+using FinansoData.Repository.Transaction;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+
+namespace FinansoData.Tests.Repository.Transaction
+{
+    public class TransactionMetaQueryRepositoryTests
+    {
+        #region GetShortListOfAllTransactionStatuses
+        [Fact]
+        public async Task GetShortListOfAllTransactionStatuses()
+        {
+            Assert.True(false);
+        }
+
+        #endregion
+
+
+        #region GetShortListOfAllTransactionTypes
+        [Fact]
+        public async Task GetShortListOfAllTransactionTypes()
+        {
+            Assert.True(false);
+        }
+
+        #endregion
+    }
+}
---
+++ FinansoData/Repository/Transaction/ITransactionMetaQueryRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace FinansoData.Repository.Transaction
+{
+    public interface ITransactionMetaQueryRepository
+    {
+        /// <summary>
+        /// Get all transaction statuses
+        /// </summary>
+        /// <returns>Tuple:
+        ///     - Id
+        ///     - Name</returns>
+        Task<RepositoryResult<IEnumerable<Tuple<int, string>>>> GetShortListOfAllTransactionStatuses();
+        /// <summary>
+        /// Get all transaction types
+        /// </summary>
+        /// <returns>Tuple:
+        ///     - Id
+        ///     - Name</returns>
+        Task<RepositoryResult<IEnumerable<Tuple<int, string>>>> GetShortListOfAllTransactionTypes();
+    }
+}
