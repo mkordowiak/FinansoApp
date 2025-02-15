470acf290393fa96aa62fa80098a0258423ff1a6
Mateusz <mateusz.kordowiak@gmail.com>
2025-02-15 23:37:37 +0100

    Repo add recurring transaction methods

--- FinansoData/Repository/Transaction/ITransactionManagementRepository.cs
+++ FinansoData/Repository/Transaction/ITransactionManagementRepository.cs
@@ -3,5 +3,7 @@
     public interface ITransactionManagementRepository
     {
         Task<RepositoryResult<bool>> AddTransaction(decimal Amount, string? Description, int BalanceId, DateTime TransactionDate, string UserName, int TransactionTypeId, int TransactionStatusId, int TransactionCategoryId);
+        Task<RepositoryResult<bool>> AddTransactionMonthlyRecurring(decimal Amount, string? Description, int BalanceId, DateTime TransactionStartDate, DateTime TransactionEndDate, string UserName, int TransactionTypeId, int TransactionCategoryId);
+        Task<RepositoryResult<bool>> AddTransactionWeeklyRecurring(decimal Amount, string? Description, int BalanceId, DateTime TransactionStartDate, DateTime TransactionEndDate, string UserName, int TransactionTypeId, int TransactionCategoryId);
     }
 }
--- FinansoData/Repository/Transaction/TransactionManagementRepository.cs
+++ FinansoData/Repository/Transaction/TransactionManagementRepository.cs
@@ -3,19 +3,116 @@ using FinansoData.Enum;
 using FinansoData.Models;
 using FinansoData.Repository.Balance;
 using Microsoft.EntityFrameworkCore;
-using System.Transactions;
 
 namespace FinansoData.Repository.Transaction
 {
     public class TransactionManagementRepository : ITransactionManagementRepository
     {
         private readonly ApplicationDbContext _applicationDbContext;
-        private readonly IBalanceManagementRepository _balanceManagmentRepository;
+        private readonly IBalanceManagementRepository _balanceManagementRepository;
 
-        public TransactionManagementRepository(ApplicationDbContext applicationDbContext, IBalanceManagementRepository balanceManagmentRepository)
+        public TransactionManagementRepository(ApplicationDbContext applicationDbContext, IBalanceManagementRepository balanceManagementRepository)
         {
             _applicationDbContext = applicationDbContext;
-            _balanceManagmentRepository = balanceManagmentRepository;
+            _balanceManagementRepository = balanceManagementRepository;
+        }
+
+        private enum RecurringTransactionTypes
+        {
+            Monthly,
+            Weekly
+        }
+
+        private List<BalanceTransaction> CreateRecuringTransactionsList(RecurringTransactionTypes recurringTransactionType, decimal Amount, string? Description, int BalanceId, DateTime TransactionStartDate, DateTime TransactionEndDate, string UserId, int TransactionTypeId, int TransactionCategoryId, int GroupId, int CurrencyId)
+        {
+            List<BalanceTransaction> balanceTransactions = new List<BalanceTransaction>();
+            string recurringTransactionId = Guid.NewGuid().ToString();
+            DateTime iterationDate = TransactionStartDate;
+
+            while (iterationDate <= TransactionEndDate)
+            {
+                BalanceTransaction newBalanceTransaction = new BalanceTransaction
+                {
+                    Amount = Amount,
+                    Description = Description,
+                    BalanceId = BalanceId,
+                    TransactionDate = iterationDate,
+                    AppUserId = UserId,
+                    TransactionTypeId = TransactionTypeId,
+                    TransactionStatusId = (int)TransactionStatuses.Planned,
+                    GroupId = GroupId,
+                    CurrencyId = CurrencyId,
+                    TransactionCategoryId = TransactionCategoryId,
+                    RecurringTransactionId = recurringTransactionId
+                };
+                balanceTransactions.Add(newBalanceTransaction);
+                if (recurringTransactionType == RecurringTransactionTypes.Monthly)
+                {
+                    iterationDate = iterationDate.AddMonths(1);
+                }
+                else if (recurringTransactionType == RecurringTransactionTypes.Weekly)
+                {
+                    iterationDate = iterationDate.AddDays(7);
+                }
+            }
+
+            return balanceTransactions;
+        }
+
+        private async Task<RepositoryResult<bool>> AddTransactionsRecuring(RecurringTransactionTypes recurringTransactionTypes, decimal Amount, string? Description, int BalanceId, DateTime TransactionStartDate, DateTime TransactionEndDate, string UserName, int TransactionTypeId, int TransactionCategoryId)
+        {
+            // this value is used in db to easly get transactions
+            var query = from balance in _applicationDbContext.Balances
+                        where balance.Id == BalanceId
+                        select new
+                        {
+                            groupId = balance.GroupId,
+                            currencyId = balance.CurrencyId
+                        };
+
+            int groupId;
+            int currencyId;
+            string? userId;
+
+
+            try
+            {
+                var data = await query.SingleOrDefaultAsync();
+
+                if (data == null)
+                {
+                    return RepositoryResult<bool>.Failure("No balance found", ErrorType.NotFound);
+                }
+
+                groupId = data.groupId;
+                currencyId = data.currencyId;
+
+                userId = await _applicationDbContext.Users.Where(u => u.UserName == UserName).Select(u => u.Id).SingleOrDefaultAsync();
+            }
+            catch
+            {
+                return RepositoryResult<bool>.Failure("Error while getting data from db", ErrorType.ServerError);
+            }
+
+            if (userId == null)
+            {
+                return RepositoryResult<bool>.Failure("No user found", ErrorType.NoUserFound);
+            }
+
+            List<BalanceTransaction> balanceTransactions = CreateRecuringTransactionsList(recurringTransactionTypes, Amount, Description, BalanceId, TransactionStartDate, TransactionEndDate, userId, TransactionTypeId, TransactionCategoryId, groupId, currencyId);
+
+
+            try
+            {
+                await _applicationDbContext.BalanceTransactions.AddRangeAsync(balanceTransactions);
+                await _applicationDbContext.SaveChangesAsync();
+            }
+            catch
+            {
+                return RepositoryResult<bool>.Failure("Error while adding transaction", ErrorType.ServerError);
+            }
+
+            return RepositoryResult<bool>.Success(true);
         }
 
         public async Task<RepositoryResult<bool>> AddTransaction(decimal Amount, string? Description, int BalanceId, DateTime TransactionDate, string UserName, int TransactionTypeId, int TransactionStatusId, int TransactionCategoryId)
@@ -73,7 +170,7 @@ namespace FinansoData.Repository.Transaction
 
             try
             {
-                _applicationDbContext.BalanceTransactions.Add(balanceTransaction);
+                await _applicationDbContext.BalanceTransactions.AddAsync(balanceTransaction);
                 await _applicationDbContext.SaveChangesAsync();
             }
             catch
@@ -86,11 +183,24 @@ namespace FinansoData.Repository.Transaction
             if (balanceTransaction.TransactionStatusId == (int)TransactionStatuses.Completed)
             {
                 decimal amountToAdd = (balanceTransaction.TransactionTypeId == (int)FinansoData.Enum.TransactionTypes.Income) ? Amount : -Amount;
-                return await _balanceManagmentRepository.AddToBalanceAmount(BalanceId, amountToAdd);
+                return await _balanceManagementRepository.AddToBalanceAmount(BalanceId, amountToAdd);
             }
 
 
             return RepositoryResult<bool>.Success(true);
+        }
+
+
+
+        public async Task<RepositoryResult<bool>> AddTransactionMonthlyRecurring(decimal Amount, string? Description, int BalanceId, DateTime TransactionStartDate, DateTime TransactionEndDate, string UserName, int TransactionTypeId, int TransactionCategoryId)
+        {
+            return await AddTransactionsRecuring(RecurringTransactionTypes.Monthly, Amount, Description, BalanceId, TransactionStartDate, TransactionEndDate, UserName, TransactionTypeId, TransactionCategoryId);
+        }
+
+
+        public async Task<RepositoryResult<bool>> AddTransactionWeeklyRecurring(decimal Amount, string? Description, int BalanceId, DateTime TransactionStartDate, DateTime TransactionEndDate, string UserName, int TransactionTypeId, int TransactionCategoryId)
+        {
+            return await AddTransactionsRecuring(RecurringTransactionTypes.Weekly, Amount, Description, BalanceId, TransactionStartDate, TransactionEndDate, UserName, TransactionTypeId, TransactionCategoryId);
         }
     }
 }
