4edb5b36dcb135c4fe932999a99d2dafa1a0521f
Mateusz <mateusz.kordowiak@gmail.com>
2025-02-15 23:36:13 +0100

    New field (RecurringTransactionId) at BalanceTransactions

---
+++ FinansoData/Migrations/20250211174811_recurring-transaction-identifier.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinansoData.Migrations
+{
+    /// <inheritdoc />
+    public partial class recurringtransactionidentifier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RecurringTransactionId",
+                table: "BalanceTransactions",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecurringTransactionId",
+                table: "BalanceTransactions");
+        }
+    }
+}
